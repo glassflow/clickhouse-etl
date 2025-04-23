1cab350e0899bbabd7c24701c2fdfcfd52f68d02
Mariam Jamal <mariam@mariamjamal.de>
2025-04-23 14:36:38 +0200

    Remove unneeded scripts

--- scripts/logs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# NOTE: hack to not have to find volume for the project always
-VOLUME="$(docker compose config | grep _logs | rev | cut -d' ' -f1 | rev)"
-LOG_LOCATION="$(docker volume inspect -f "{{ .Mountpoint }}" "$VOLUME")"
-
-printf "Log location: %s\n\n" "$LOG_LOCATION"
-
-cat "$LOG_LOCATION/$(ls "$LOG_LOCATION" | tail -n 1)"
