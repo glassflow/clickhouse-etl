ba887bb91325e466b7810892d5ceac3633d203d3
Vladimir Cutkovic <vladimir@glassflow.dev>
2025-05-09 16:29:37 +0200

    feat: add event context to join + dedup journey

--- ui/src/modules/TopicDeduplicationSwitch2.tsx
+++ ui/src/modules/TopicDeduplicationSwitch2.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { TopicDeduplicationConfigurator, TopicDeduplicationConfiguratorProps } from './TopicDeduplicationConfigurator'
+import { EventFetchProvider } from './event-fetcher/EventFetchContext'
 
 export const TopicDeduplicationSwitch2 = ({
   steps,
@@ -7,5 +8,9 @@ export const TopicDeduplicationSwitch2 = ({
   validate,
   index = 1,
 }: TopicDeduplicationConfiguratorProps & { index: number }) => {
-  return <TopicDeduplicationConfigurator steps={steps} onNext={onNext} validate={validate} index={index} />
+  return (
+    <EventFetchProvider>
+      <TopicDeduplicationConfigurator steps={steps} onNext={onNext} validate={validate} index={index} />
+    </EventFetchProvider>
+  )
 }
