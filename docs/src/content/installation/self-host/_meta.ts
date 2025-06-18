a90c3155a481c14aca154d406815e4508eb2ead8
Ashish Bagri <ashishbagri@gmail.com>
2025-06-18 15:25:11 +0200

    WIP: add additional deployment pages

---
+++ docs/src/content/installation/self-host/_meta.ts
@@ -0,0 +1,6 @@
+export default {
+  "local-docker": { "title": "Local Docker" },
+  "aws-ec2": { "title": "AWS EC2" },
+  "kubernetes-helm": { "title": "Kubernetes with Helm" },
+  "gcp-pulumi": { "title": "GCP with Pulumi" },
+};
