5630137c9889248df410a1ed8de8c03f4013bbfb
Ashish Bagri <ashishbagri@gmail.com>
2025-06-19 09:56:38 +0200

    Updated k8s docs

--- docs/src/content/installation/self-host/_meta.ts
+++ docs/src/content/installation/self-host/_meta.ts
@@ -1,6 +1,5 @@
 export default {
   "local-docker": { "title": "Local Docker" },
   "aws-ec2": { "title": "AWS EC2" },
-  "kubernetes-helm": { "title": "Kubernetes with Helm" },
-  "gcp-pulumi": { "title": "GCP with Pulumi" },
+  "kubernetes-helm": { "title": "Kubernetes with Helm" }
 };
