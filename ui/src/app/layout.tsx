5988fcaf91bd3efe060141f490c57150561f09f3
Vladimir Cutkovic <vladimir@glassflow.dev>
2025-06-19 12:59:35 +0200

    feat: add top header and navigation structure

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from 'next'
 import ThemeProvider from '@/src/components/shared/ThemeProvider'
 import { Inter, Archivo } from 'next/font/google'
 import './globals.css'
-import { HeaderStandalone } from '../components/shared/HeaderStandalone'
+import { Header } from '../components/shared/Header'
+import { HeaderWrapper } from '../components/shared/HeaderWrapper'
 import { Button } from '@/src/components/ui/button'
 import GlobalFooter from '@/src/components/shared/GlobalFooter'
 import { ConsentLayout } from '@/src/components/layout/ConsentLayout'
@@ -37,21 +38,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Script src="/env.js" strategy="beforeInteractive" />
         <ThemeProvider>
           <ConsentLayout>
-            <div className="grid grid-rows-[auto_1fr_auto] h-screen max-h-screen w-full overflow-auto">
-              <header className="w-full px-8 sm:px-20 pt-4 shrink-0">
-                <div className="max-w-[var(--main-container-width)] mx-auto w-full">
-                  <HeaderStandalone />
-                </div>
-              </header>
-              <main className="flex flex-col items-center w-full px-8 sm:px-20 py-8 mt-16 overflow-y-auto">
-                <div className="max-w-[1240px] w-full">{children}</div>
-              </main>
-              <footer className="w-full px-8 sm:px-20 py-6 shrink-0">
-                <div className="max-w-[1240px] mx-auto w-full flex gap-6 flex-wrap items-center justify-center">
-                  <GlobalFooter />
-                </div>
-              </footer>
-            </div>
+            <HeaderWrapper>
+              <Header />
+            </HeaderWrapper>
+            <main className="flex flex-col items-center w-full px-8 sm:px-20 py-8 overflow-y-auto">
+              <div className="max-w-[1240px] w-full">{children}</div>
+            </main>
+            <footer className="w-full px-8 sm:px-20 py-6 shrink-0">
+              <div className="max-w-[1240px] mx-auto w-full flex gap-6 flex-wrap items-center justify-center">
+                <GlobalFooter />
+              </div>
+            </footer>
           </ConsentLayout>
         </ThemeProvider>
       </body>
--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,12 +1,8 @@
-import { Header } from '@/src/components/shared/Header'
-
 export default function Home() {
   return (
     // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] min-w-[1200px] max-w-[1200px] mx-auto">
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] min-w-[var(--main-container-width)] max-w-[var(--main-container-width)] mx-auto">
-      <header className="row-start-1 w-full">
-        <Header />
-      </header>
+      <header className="row-start-1 w-full"></header>
       <main className="flex flex-col gap-[32px] row-start-2 items-center">{/* Your page content goes here */}</main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center w-full">
         <p>Glassflow Footer</p>
