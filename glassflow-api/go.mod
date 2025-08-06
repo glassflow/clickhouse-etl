e53c5c6a856eb5729204355f9a47f07f746dd101
mj <mariam@mariamjamal.de>
2025-08-06 17:36:14 +0200

    Implement k8s orchestrator setup pipeline

--- glassflow-api/go.mod
+++ glassflow-api/go.mod
@@ -18,6 +18,8 @@ require (
 	github.com/testcontainers/testcontainers-go v0.37.0
 	github.com/testcontainers/testcontainers-go/modules/clickhouse v0.35.0
 	github.com/xdg-go/scram v1.1.1
+	k8s.io/apimachinery v0.33.3
+	sigs.k8s.io/controller-runtime v0.21.0
 )
 
 require (
@@ -53,15 +55,23 @@ require (
 	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
 	github.com/eapache/queue v1.1.0 // indirect
 	github.com/ebitengine/purego v0.8.2 // indirect
+	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
+	github.com/evanphx/json-patch/v5 v5.9.11 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/go-faster/city v1.0.1 // indirect
 	github.com/go-faster/errors v0.7.1 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/gofrs/uuid v4.3.1+incompatible // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/gnostic-models v0.6.9 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
 	github.com/hashicorp/go-memdb v1.3.4 // indirect
@@ -73,16 +83,22 @@ require (
 	github.com/jcmturner/gofork v1.7.6 // indirect
 	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/magiconair/properties v1.8.10 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/patternmatcher v0.6.0 // indirect
 	github.com/moby/sys/sequential v0.5.0 // indirect
 	github.com/moby/sys/user v0.1.0 // indirect
 	github.com/moby/sys/userns v0.1.0 // indirect
 	github.com/moby/term v0.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/nats-io/nkeys v0.4.9 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
@@ -100,11 +116,12 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/stringprep v1.0.3 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.58.0 // indirect
 	go.opentelemetry.io/otel v1.35.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.35.0 // indirect
 	go.opentelemetry.io/otel/metric v1.35.0 // indirect
@@ -113,10 +130,23 @@ require (
 	go.opentelemetry.io/proto/otlp v1.5.0 // indirect
 	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/oauth2 v0.27.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/term v0.32.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
+	golang.org/x/time v0.9.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/api v0.33.0 // indirect
+	k8s.io/client-go v0.33.0 // indirect
+	k8s.io/klog/v2 v2.130.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20250318190949-c8a335a9a2ff // indirect
+	k8s.io/utils v0.0.0-20241104100929-3ea5e8cea738 // indirect
+	sigs.k8s.io/json v0.0.0-20241010143419-9aa6b5e7a4b3 // indirect
+	sigs.k8s.io/randfill v1.0.0 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.6.0 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
 )
 
 replace (
--- glassflow-api/internal/orchestrator/k8s.go
+++ glassflow-api/internal/orchestrator/k8s.go
@@ -2,30 +2,135 @@ package orchestrator
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log/slog"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
 
 	"github.com/glassflow/clickhouse-etl-internal/glassflow-api/internal/models"
 	"github.com/glassflow/clickhouse-etl-internal/glassflow-api/internal/service"
 )
 
-// TODO: add k8s client
 type K8sOrchestrator struct {
-	log *slog.Logger
+	client         *dynamic.DynamicClient
+	log            *slog.Logger
+	customResource CustomResourceAPIGroupVersion
+}
+
+type CustomResourceAPIGroupVersion struct {
+	Resource string
+	Kind     string
+	APIGroup string
+	Version  string
 }
 
 func NewK8sOrchestrator(
 	log *slog.Logger,
-) service.Orchestrator {
-	return &K8sOrchestrator{
-		log: log,
+	agv CustomResourceAPIGroupVersion,
+) (service.Orchestrator, error) {
+	kcfg, err := config.GetConfig()
+	if err != nil {
+		return nil, fmt.Errorf("unable to get kubeconfig: %w", err)
 	}
+
+	log.Info("connected to cluster", slog.String("kubeconfig", kcfg.Host))
+
+	client, err := dynamic.NewForConfig(kcfg)
+	if err != nil {
+		return nil, fmt.Errorf("new k8s client: %w", err)
+	}
+
+	return &K8sOrchestrator{
+		client:         client,
+		log:            log,
+		customResource: agv,
+	}, nil
 }
 
 var _ service.Orchestrator = (*K8sOrchestrator)(nil)
 
 // SetupPipeline implements Orchestrator.
-func (k *K8sOrchestrator) SetupPipeline(_ context.Context, _ *models.PipelineConfig) error {
-	panic("unimplemented")
+func (k *K8sOrchestrator) SetupPipeline(ctx context.Context, cfg *models.PipelineConfig) error {
+	src := make([]sourceStreams, 0, len(cfg.Ingestor.KafkaTopics))
+
+	for _, s := range cfg.Ingestor.KafkaTopics {
+		src = append(src, sourceStreams{
+			TopicName:    s.Name,
+			OutputStream: s.Name,
+			DedupWindow:  s.Deduplication.Window.Duration(),
+		})
+	}
+
+	pcfg, err := json.Marshal(*cfg)
+	if err != nil {
+		return fmt.Errorf("marshal pipeline config: %w", err)
+	}
+
+	spec := pipelineSpec{
+		ID:  cfg.ID,
+		DLQ: models.GetDLQStreamName(cfg.ID),
+		Ingestor: sources{
+			Type:    cfg.Ingestor.Type,
+			Streams: src,
+		},
+		Join: join{
+			Type:         "temporal",
+			OutputStream: models.GetJoinedStreamName(cfg.ID),
+			Enabled:      cfg.Join.Enabled,
+		},
+		Sink:   cfg.Sink.Type,
+		Config: string(pcfg),
+	}
+
+	var specMap map[string]any
+
+	jsonSpec, err := json.Marshal(spec)
+	if err != nil {
+		return fmt.Errorf("marshal k8s pipeline spec: %w", err)
+	}
+
+	err = json.Unmarshal(jsonSpec, &specMap)
+	if err != nil {
+		return fmt.Errorf("unmarshal k8s pipeline spec to spec map: %w", err)
+	}
+
+	obj := &unstructured.Unstructured{
+		Object: map[string]any{
+			"metadata": map[string]any{
+				"name": cfg.ID,
+			},
+			"spec": specMap,
+		},
+	}
+
+	obj.SetGroupVersionKind(schema.GroupVersionKind{
+		Group:   k.customResource.APIGroup,
+		Version: k.customResource.Version,
+		Kind:    k.customResource.Kind,
+	})
+
+	//TODO: discuss namespace
+	//nolint: exhaustruct // optional config
+	_, err = k.client.Resource(schema.GroupVersionResource{
+		Group:    k.customResource.APIGroup,
+		Version:  k.customResource.Version,
+		Resource: k.customResource.Resource,
+	}).
+		Namespace("default").
+		Create(ctx, obj, metav1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("create custom resource: %w", err)
+	}
+
+	k.log.Info("created k8s pipeline", slog.String("pipeline_id", cfg.ID))
+
+	return nil
 }
 
 // ShutdownPipeline implements Orchestrator.
@@ -33,3 +138,30 @@ func (k *K8sOrchestrator) ShutdownPipeline(_ context.Context, _ string) error {
 	panic("unimplemented")
 }
 
+// TODO: include via operator library instead of copy pasting
+// pipelineSpec defines the desired custom resource
+type pipelineSpec struct {
+	ID       string  `json:"pipeline_id"`
+	DLQ      string  `json:"dlq"`
+	Ingestor sources `json:"sources"`
+	Join     join    `json:"join"`
+	Sink     string  `json:"sink"`
+	Config   string  `json:"config"`
+}
+
+type sources struct {
+	Type    string          `json:"type"`
+	Streams []sourceStreams `json:"topics"`
+}
+
+type sourceStreams struct {
+	TopicName    string        `json:"topic_name"`
+	OutputStream string        `json:"stream"`
+	DedupWindow  time.Duration `json:"dedup_window"`
+}
+
+type join struct {
+	Type         string `json:"type"`
+	OutputStream string `json:"stream"`
+	Enabled      bool   `json:"enabled"`
+}
