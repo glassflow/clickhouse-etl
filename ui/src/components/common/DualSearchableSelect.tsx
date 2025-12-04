b73d8b45c494f2cda00f3e1ab5eabbf247e87ffc
Vladimir Cutkovic <vladimir@glassflow.dev>
2025-12-04 21:38:27 +0100

    align components with figma designs consolidate layered css remove light theme files restyle inputs, selects and home page

--- ui/src/components/common/DualSearchableSelect.tsx
+++ ui/src/components/common/DualSearchableSelect.tsx
@@ -285,7 +285,7 @@ export function DualSearchableSelect({
         typeof window !== 'undefined' &&
         createPortal(
           <div
-            className="fixed z-50 shadow-md rounded-md overflow-hidden bg-[#1e1e1f] flex"
+            className="fixed z-50 shadow-md rounded-md overflow-hidden bg-[var(--color-black-300,#1e1e1f)] border border-[var(--color-border-regular,#373737)] flex"
             style={{
               top: dropdownCoordinates.top + 'px',
               left: dropdownCoordinates.left + 'px',
