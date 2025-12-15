82476380636c6af9242104e071300411bb81025b
Vladimir Cutkovic <vladimir@glassflow.dev>
2025-12-15 23:20:30 +0100

    improve design of filter composer

--- ui/src/modules/filter/components/QueryRule.tsx
+++ ui/src/modules/filter/components/QueryRule.tsx
@@ -323,7 +323,7 @@ export function QueryRule({
   return (
     <div
       className={cn(
-        'card-outline rounded-[var(--radius-medium)] p-3',
+        'card-outline rounded-[var(--radius-large)] p-3',
         rule.not && 'border-[var(--color-border-primary)] bg-[var(--color-background-primary-faded)]/10',
       )}
     >
