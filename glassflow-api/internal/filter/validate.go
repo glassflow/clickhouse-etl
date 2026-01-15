30cc966db46bcc22156e1166b514132a9b624af1
Petr Nuzhnov <petr@glassflow.dev>
2026-01-15 16:48:02 +0100

    Change pipeline config construction routine for CREATE api

--- glassflow-api/internal/filter/validate.go
+++ glassflow-api/internal/filter/validate.go
@@ -49,3 +49,44 @@ func ValidateFilterExpression(expression string, fields []models.StreamDataField
 
 	return nil
 }
+
+// Temporary function to validate filter expression against v2 schema fields
+func ValidateFilterExpressionV2(expression string, fields []models.Field) error {
+	if expression == "" {
+		return fmt.Errorf("empty expression")
+	}
+
+	jsonInput := ""
+	for _, field := range fields {
+		defaultTypeValue, err := schema.GetDefaultValueForKafkaType(schema.KafkaDataType(field.Type))
+		if err != nil {
+			return fmt.Errorf("get default value for field %s: %w", field.Type, err)
+		}
+
+		jsonInput, err = sjson.Set(jsonInput, field.Name, defaultTypeValue)
+		if err != nil {
+			return fmt.Errorf("failed to set field")
+		}
+	}
+	exprEnv := make(map[string]interface{})
+	err := json.Unmarshal([]byte(jsonInput), &exprEnv)
+	if err != nil {
+		return fmt.Errorf("unmarshal json: %w", err)
+	}
+
+	compiledExecutor, err := expr.Compile(expression, expr.Env(exprEnv))
+	if err != nil {
+		return fmt.Errorf("compile expression: %w", err)
+	}
+
+	result, err := expr.Run(compiledExecutor, exprEnv)
+	if err != nil {
+		return fmt.Errorf("eval expression: %w", err)
+	}
+
+	if _, ok := result.(bool); !ok {
+		return fmt.Errorf("expression evaluated to %t", result)
+	}
+
+	return nil
+}
