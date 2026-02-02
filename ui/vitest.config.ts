6862a251de0135a72682927ccc5d2ecb8b641f8f
Vladimir Cutkovic <vladimir@glassflow.dev>
2026-02-02 19:00:24 +0100

    minor deduplication refactor and add basic tests

--- ui/vitest.config.ts
+++ ui/vitest.config.ts
@@ -16,6 +16,8 @@ export default defineConfig({
       'test/**/*.{test,spec}.{ts,tsx}',
       'modules/create/**/*.{test,spec}.{ts,tsx}',
       'modules/kafka/**/*.{test,spec}.{ts,tsx}',
+      'modules/deduplication/**/*.{test,spec}.{ts,tsx}',
+      'store/**/*.{test,spec}.{ts,tsx}',
       'utils/**/*.{test,spec}.{ts,tsx}',
     ],
     exclude: ['node_modules', '.next'],
