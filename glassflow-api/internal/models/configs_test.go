aaadce21babb93774f3901a98ad1ec5c7d4e0a98
Kiran <nkirandroid@gmail.com>
2026-03-11 16:53:09 +0100

    fix: update unit tests for stream prefix

--- glassflow-api/internal/models/configs_test.go
+++ glassflow-api/internal/models/configs_test.go
@@ -147,17 +147,17 @@ func TestGetPipelineStreamName(t *testing.T) {
 		{
 			name:     "simple topic",
 			topic:    "my-topic",
-			expected: "gf-", // Will be followed by hash and topic
+			expected: "gfm-", // Will be followed by hash and topic
 		},
 		{
 			name:     "topic with dots",
 			topic:    "my.topic.name",
-			expected: "gf-", // Will be followed by hash and sanitized topic
+			expected: "gfm-", // Will be followed by hash and sanitized topic
 		},
 		{
 			name:     "very long topic name",
 			topic:    strings.Repeat("a", 50),
-			expected: "gf-", // Should be truncated
+			expected: "gfm-", // Should be truncated
 		},
 	}
 
@@ -198,17 +198,17 @@ func TestGetIngestorStreamName(t *testing.T) {
 		{
 			name:     "simple topic",
 			topic:    "my-topic",
-			expected: "gf-", // Will be followed by hash and topic
+			expected: "gfm-", // Will be followed by hash and topic
 		},
 		{
 			name:     "topic with dots",
 			topic:    "my.topic.name",
-			expected: "gf-", // Will be followed by hash and sanitized topic
+			expected: "gfm-", // Will be followed by hash and sanitized topic
 		},
 		{
 			name:     "very long topic name",
 			topic:    strings.Repeat("a", 50),
-			expected: "gf-", // Should be truncated
+			expected: "gfm-", // Should be truncated
 		},
 	}
 
