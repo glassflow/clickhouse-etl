a221ecae464b6b1e52e6c3ed4c237b409403cfe5
nnaumov-glassflow <nikita@glassflow.dev>
2026-03-26 11:11:14 +0100

    wire http and grpc handlers to otlp logic

--- glassflow-api/cmd/glassflow/oltp_receiver.go
+++ glassflow-api/cmd/glassflow/oltp_receiver.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 
 	"github.com/glassflow/clickhouse-etl-internal/glassflow-api/internal/client"
+	"github.com/glassflow/clickhouse-etl-internal/glassflow-api/internal/models"
 	oltp_receiver "github.com/glassflow/clickhouse-etl-internal/glassflow-api/internal/otlp-receiver/server"
 	otlp_processor "github.com/glassflow/clickhouse-etl-internal/glassflow-api/internal/otlp-receiver/server/processor"
 )
@@ -15,11 +16,27 @@ func mainOLTPReceiver(
 	_ *config,
 	log *slog.Logger,
 ) error {
-	otlpDataProcessor := otlp_processor.NewProcessor(nil, nc)
+	configFetcher := configFetcherStub{}
+	otlpDataProcessor := otlp_processor.NewProcessor(configFetcher, nc)
 	r, err := oltp_receiver.New(log, otlpDataProcessor)
 	if err != nil {
 		return err
 	}
 	defer r.Shutdown(context.Background())
 	return r.Start(ctx)
+}
+
+type configFetcherStub struct {
+}
+
+func (c configFetcherStub) GetOTLPConfig(
+	_ context.Context,
+	_ string,
+) (models.OTLPConfig, error) {
+	return models.OTLPConfig{
+		Routing: models.RoutingConfig{
+			OutputSubject: "otlp-test",
+			Type:          models.RoutingTypeName,
+		},
+	}, nil
 }
