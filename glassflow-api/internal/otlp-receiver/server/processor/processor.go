542791b79029c22c75239810f6ad5f2ace724e8c
nnaumov-glassflow <nikita@glassflow.dev>
2026-03-27 16:53:51 +0100

    setup nats dedup header in otlp receiver

--- glassflow-api/internal/otlp-receiver/server/processor/processor.go
+++ glassflow-api/internal/otlp-receiver/server/processor/processor.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/avast/retry-go"
+	"github.com/tidwall/gjson"
 
 	"github.com/glassflow/clickhouse-etl-internal/glassflow-api/internal/batch"
 	"github.com/glassflow/clickhouse-etl-internal/glassflow-api/internal/batch/nats"
@@ -28,61 +29,67 @@ func NewProcessor(
 ) *Processor {
 	return &Processor{
 		otlpConfigFetcher: otlpConfigFetcher,
-		natsWriterCache:   make(map[string]cacheEntry),
+		natsWriterCache:   make(map[string]writerConfig),
 		nc:                nc,
 	}
 }
 
-const natsWriterCacheTTL = 15 * time.Second
+const natsWriterCacheTTL = 60 * time.Second
 
-type cacheEntry struct {
-	writer    batch.BatchWriter
-	expiresAt time.Time
+type writerConfig struct {
+	nats          batch.BatchWriter
+	routingConfig models.OTLPConfig
+	expiresAt     time.Time
 }
 
 type Processor struct {
 	otlpConfigFetcher OTLPConfigFetcher
-	natsWriterCache   map[string]cacheEntry
+	natsWriterCache   map[string]writerConfig
 	natsWriterMu      sync.RWMutex
 	nc                *client.NATSClient
 }
 
-func (p *Processor) getNatsWriter(
+func (p *Processor) getWriterConfig(
 	ctx context.Context,
 	pipelineID string,
-) (batch.BatchWriter, error) {
+) (writerConfig, error) {
 	p.natsWriterMu.RLock()
 	entry, ok := p.natsWriterCache[pipelineID]
 	p.natsWriterMu.RUnlock()
 	if ok && !entry.expiresAt.Before(time.Now()) {
-		return entry.writer, nil
+		return entry, nil
 	}
 
 	otlpConfig, err := p.otlpConfigFetcher.GetOTLPConfig(ctx, pipelineID)
 	if err != nil {
-		return nil, fmt.Errorf("otlp config fetch: %w", err)
+		return writerConfig{}, fmt.Errorf("otlp config fetch: %w", err)
 	}
 
 	subjectRouter, err := subjectrouter.New(otlpConfig.Routing)
 	if err != nil {
-		return nil, fmt.Errorf("subjectrouter.New: %w", err)
+		return writerConfig{}, fmt.Errorf("subjectrouter.New: %w", err)
 	}
 
-	newWriter := nats.NewBatchWriter(p.nc.JetStream(), subjectRouter)
+	newWriterConfig := nats.NewBatchWriter(p.nc.JetStream(), subjectRouter)
 
 	p.natsWriterMu.Lock()
 	defer p.natsWriterMu.Unlock()
 
 	if p.natsWriterCache == nil {
-		p.natsWriterCache = make(map[string]cacheEntry)
+		p.natsWriterCache = make(map[string]writerConfig)
 	}
 
 	if entry, ok := p.natsWriterCache[pipelineID]; ok && !entry.expiresAt.Before(time.Now()) {
-		return entry.writer, nil
+		return entry, nil
 	}
 
-	p.natsWriterCache[pipelineID] = cacheEntry{writer: newWriter, expiresAt: time.Now().Add(natsWriterCacheTTL)}
-	return newWriter, nil
+	p.natsWriterCache[pipelineID] = writerConfig{
+		nats:          newWriterConfig,
+		routingConfig: otlpConfig,
+		expiresAt:     time.Now().Add(natsWriterCacheTTL),
+	}
+
+	return p.natsWriterCache[pipelineID], nil
 }
 
 func (p *Processor) invalidateNatsWriter(pipelineID string) {
@@ -99,15 +106,23 @@ func (p *Processor) sendBatch(
 ) error {
 	err := retry.Do(
 		func() error {
-			natsWriter, err := p.getNatsWriter(ctx, pipelineID)
+			cfg, err := p.getWriterConfig(ctx, pipelineID)
 			if err != nil {
 				if errors.Is(err, service.ErrPipelineNotFound) {
 					return retry.Unrecoverable(service.ErrPipelineNotFound)
 				}
-				return fmt.Errorf("getNatsWriter: %w", err)
+				return fmt.Errorf("getWriterConfig: %w", err)
 			}
 
-			failedMessages := natsWriter.WriteBatch(ctx, messages)
+			// for now RoutingTypeField will be used for dedup based routing
+			if cfg.routingConfig.Routing.Type == models.RoutingTypeField {
+				messages, err = setupNatsDedupHeader(cfg, messages)
+				if err != nil {
+					return retry.Unrecoverable(err)
+				}
+			}
+
+			failedMessages := cfg.nats.WriteBatch(ctx, messages)
 			if len(failedMessages) > 0 {
 				p.invalidateNatsWriter(pipelineID)
 				messages = extractMessages(failedMessages)
@@ -140,4 +155,20 @@ func extractMessages(failed []models.FailedMessage) []models.Message {
 		msgs[i] = fm.Message
 	}
 	return msgs
+}
+
+func setupNatsDedupHeader(cfg writerConfig, messages []models.Message) ([]models.Message, error) {
+	for i := range messages {
+		dedupValue := gjson.GetBytes(
+			messages[i].Payload(),
+			cfg.routingConfig.Routing.Field.Name,
+		).String()
+
+		if dedupValue == "" {
+			return nil, errors.New("dedup value is empty")
+		}
+		messages[i].SetHeader("Nats-Msg-Id", dedupValue)
+	}
+
+	return messages, nil
 }
